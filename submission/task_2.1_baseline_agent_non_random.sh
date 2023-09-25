9230bced5f4fd5eab9b83b108166b918dafab955
James Stankowicz <james.stankowicz@str.us>
2023-09-25 15:11:53 -0400

    Submit to non-random environment.

--- submission/task_2.1_baseline_agent_non_random.sh
+++ submission/task_2.1_baseline_agent_non_random.sh
@@ -1,10 +1,9 @@
 python multigrid/scripts/train.py \
  --local-mode False \
- --env MultiGrid-CompetativeRedBlueDoor-v3-CTDE-Red \
+ --env MultiGrid-CompetativeRedBlueDoor-v3-CTDE-Red-NonRandom \
  --num-workers 10 \
  --num-gpus 0 \
  --name CTDE-Red_baseline \
  --training-scheme CTDE \
  --training-config '{"team_policies_mapping": {"red_0" : "your_policy_name" , "red_1" : "your_policy_name_v2" }}' \
- --restore-all-policies-from-checkpoint False \
- --randomization False
+ --restore-all-policies-from-checkpoint False
