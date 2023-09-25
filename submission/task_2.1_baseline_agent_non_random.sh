66e40679d6573b2961a7227a21446d74a4652c79
James Stankowicz <james.stankowicz@str.us>
2023-09-25 15:01:37 -0400

    Create non-random agent training start script.

---
+++ submission/task_2.1_baseline_agent_non_random.sh
@@ -0,0 +1,10 @@
+python multigrid/scripts/train.py \
+ --local-mode False \
+ --env MultiGrid-CompetativeRedBlueDoor-v3-CTDE-Red \
+ --num-workers 10 \
+ --num-gpus 0 \
+ --name CTDE-Red_baseline \
+ --training-scheme CTDE \
+ --training-config '{"team_policies_mapping": {"red_0" : "your_policy_name" , "red_1" : "your_policy_name_v2" }}' \
+ --restore-all-policies-from-checkpoint False \
+ --randomization False
